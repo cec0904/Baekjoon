72deeab823ce8608a11eb8ef486cc5d92be45763
cec0904 <163234162+cec0904@users.noreply.github.com>
2025-04-08 09:36:22 +0900

    [Bronze V] Title: 문자열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/9086. 문자열/문자열.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	string str;
+	int i;
+
+	cin >> i;
+	
+
+	for (int j = 0; j < i; j++) {
+		cin >> str;
+
+		cout << str.front() << str.back() << endl;
+
+
+	
+	}
+
+
+}
