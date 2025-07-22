48d23056a880256d2726e5e7d40b1957008216af
cec0904 <163234162+cec0904@users.noreply.github.com>
2025-07-22 14:12:08 +0900

    [level 0] Title: 소문자로 바꾸기, Time: 0.01 ms, Memory: 4.16 MB -BaekjoonHub

---
+++ 프로그래머스/0/181876. 소문자로 바꾸기/소문자로 바꾸기.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+string solution(string myString) {
+    string answer = "";
+    
+    transform(myString.begin(), myString.end(), myString.begin(), ::tolower);
+    
+    return myString;
+}
